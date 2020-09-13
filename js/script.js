1e626ef6704cbe0535481e17aa210c9af7750177
Dutch <j.breda@outlook.com>
2020-09-13 15:32:18 -0400

    Added modal, but...
    Tried everything to make the pokemons appear on the site and add modal to these buttons. Played around with the examples and changed mmany times my code. Still can't figure it out

--- js/script.js
+++ js/script.js
@@ -76,12 +76,84 @@ function loadDetails(item) {
   }
 
 // Function to show details of each pokemon in alert window:
+//  function showDetails(item) {
+//    pokemonRepository.loadDetails(item).then(function () {
+//    console.log(item);
+//    alert('Height:' + ' ' + item.height + ' ' + 'Type: ' + ' ' + item.types);
+//    });
+//  }
+
+// Show modal content
   function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function () {
-    console.log(item);
-    alert('Height:' + ' ' + item.height + ' ' + 'Type: ' + ' ' + item.types);
-    });
+    var $modalContainer = $('#modal-container');
+    //clear existing content of the model
+    $modalContainer.empty();
+    //creating div element in DOM
+    //adding class to div DOM element
+    var modal = $('<div class="modal"></div>');
+    //creating closing button in modal content
+    var closeButtonElement = $('<button class="modal-close">Close</button>');
+    // Closing modal after click
+    closeButtonElement.on('click', hideModal);
+    //creating element for name in modal content
+    var nameElement = $('<h1>' + item.name + '</h1>');
+    // creating img in modal content
+    var imageElement = $('<img class="modal-img">');
+    imageElement.attr('src', item.imageUrl);
+    //creating element for height in modal content
+    var heightElement = $('<p>' + 'height : ' + item.height + 'm' + '</p>');
+    //creating element for weight in modal content
+    var weightElement = $('<p>' + 'weight : ' + item.weight + 'kg' + '</p>');
+    //creating element for type in modal content
+    var typesElement = $('<p>' + 'types : ' + item.types + '</p>');
+    //creating element for abilities in modal content
+    var abilitiesElement = $('<p>' + 'abilities : ' + item.abilities + '</p>');
+    // Append modal content to webpage
+    modal.append(closeButtonElement);
+    modal.append(nameElement);
+    modal.append(imageElement);
+    modal.append(heightElement);
+    modal.append(weightElement);
+    modal.append(typesElement);
+    modal.append(abilitiesElement);
+    $modalContainer.append(modal);
+    // Add class to show modal
+    $modalContainer.addClass('is-visible');
   }
+
+  // Hides modal on click
+  function hideModal() {
+    var $modalContainer = $('#modal-container');
+    $modalContainer.removeClass('is-visible');
+  }
+
+  // Hides model with ESC
+  jQuery(window).on('keydown', e => {
+    var $modalContainer = $('#modal-container');
+    if (e.key === 'Escape' && $modalContainer.hasClass('is-visible')) {
+      hideModal();
+    }
+  });
+
+  // Hides modal if clicked outside Modal
+  var $modalContainer = document.querySelector('#modal-container');
+  $modalContainer.addEventListener('click', e => {
+    var target = e.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showModal: showModal,
+    hideModal: hideModal
+  };
+})();
 
 // To return the values wich can be accessed to outside the IIFE:
   return {
